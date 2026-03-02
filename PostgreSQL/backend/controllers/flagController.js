3fd8d2816858af1d3ab55b45712a64ac9845a18c
Diego <diegofoeppel@gmail.com>
2026-03-02 14:06:14 -0300

    modificacao no flag controller

--- PostgreSQL/backend/controllers/flagController.js
+++ PostgreSQL/backend/controllers/flagController.js
@@ -10,8 +10,15 @@ const getRandomFlag = async (req, res) => {
     const result = await db.query(query);
     console.log("result", result.rows);
 
+    const question = result.rows[0];
+
+    const currentQuestion = {
+      ...question,
+      flagString: emojiCountryCode(question.flag),
+    };
+
     // const response = await db.query("select * from capitals");
-    res.send({ success: true, data: result.rows[0] });
+    res.send({ success: true, data: currentQuestion });
   } catch (err) {
     res.send({ success: false, error: err.stack });
   }
@@ -28,6 +35,8 @@ const checkFlag = async (req, res) => {
 
     const result = await db.query(query);
 
+    const question = result.rows[0];
+
     res.send(result.rows[0]);
   } catch (err) {
     console.error(err);
@@ -35,11 +44,20 @@ const checkFlag = async (req, res) => {
   }
 };
 
-const nextQuestion = () => {
-  //   const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
-  const randomFlag = getRandomFlag();
+function emojiCountryCode(flag) {
+  const formattedCode =
+    String.fromCharCode(flag.codePointAt(0) - 127397) +
+    String.fromCharCode(flag.codePointAt(2) - 127397);
+  return formattedCode.toLowerCase();
+}
 
-  currentQuestion = randomFlag;
-};
+async function nextQuestion() {
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+
+  currentQuestion = {
+    ...randomCountry,
+    flagString: emojiCountryCode(randomCountry.flag),
+  };
+}
 
 export { getRandomFlag, checkFlag };
